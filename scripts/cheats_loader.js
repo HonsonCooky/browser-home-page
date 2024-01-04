667fda3d94f04fe6be21c4540e2b626e5b1587a5
Harrison Cook <HonsonCooky@gmail.com>
2024-01-04 14:38:44 +1300

    start browser cheats

---
+++ scripts/cheats_loader.js
@@ -0,0 +1,1 @@
+import edge_json from "../assets/edge.json";
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 import "./scroll_responder";
 import "./theme_responder";
-import * as file_reader from "./file_reader";
+import "./cheats_loader";
